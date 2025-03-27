2d242f13f2f506793269eda39dc256e5487456de
ViniciusG.Ribeiro <viniciusrib2014@hotmail.com>
2025-03-27 15:33:23 -0300

    atualizando o prjeto para rodar no GH

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/product-list-with-cart/",
   plugins: [react()],
   //   server: {
   //   host: true,
